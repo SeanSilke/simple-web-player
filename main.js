726b1152624797cac500f305ac451021b80f55b1
Sergey Orlov <OrlovMail@gmail.com>
2015-07-27 19:20:08 +0300

    Added volume control

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-var context;
+// define variables
+var context = new (window.AudioContext || window.webkitAudioContext);;
+var source;
+var gainNode;
 var soundBuffer = null;
-var source ;
 
-window.addEventListener('load', init, false);
-
-function init() {
-    try {
-        context = new (window.AudioContext || window.webkitAudioContext);
-    }
-    catch (e) {
-        alert('Web Audio API is not supported in this browser');
-    }
-    aReq.send();
-}
+var volumeControl = document.querySelector('.volume-control');
+///
 
 var aReq = new XMLHttpRequest();
 aReq.responseType = 'arraybuffer'
 aReq.onload = reqListener;
 aReq.open("get", "samples/Onze-20 - Joao e Grazi.mp3", true);
-
+aReq.send();
 
 function reqListener() {
     context.decodeAudioData(aReq.response, function (buffer) {
@@ -30,6 +23,14 @@ function reqListener() {
 function playSound(buffer) {
     source = context.createBufferSource();
     source.buffer = soundBuffer;
-    source.connect(context.destination);
+    gainNode = context.createGain();
+    source.connect(gainNode);
+    gainNode.connect(context.destination);
+    gainNode.gain.value = volumeControl.value;
     source.start(0);
+}
+
+// wire up buttons
+volumeControl.oninput = function(){
+    gainNode.gain.value = this.value;
 }
