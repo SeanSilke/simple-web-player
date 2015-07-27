621a5341028184243ae9b7188573dafe6ac2ee22
Sergey Orlov <OrlovMail@gmail.com>
2015-07-27 20:58:41 +0300

    Added play and stop buttons

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 // define variables
-var context = new (window.AudioContext || window.webkitAudioContext);;
+var context = new (window.AudioContext || window.webkitAudioContext);
 var source;
 var gainNode;
+
 var soundBuffer = null;
 
 var volumeControl = document.querySelector('.volume-control');
+var stop = document.querySelector('.stop');
+var play = document.querySelector('.play');
 ///
 
-var aReq = new XMLHttpRequest();
-aReq.responseType = 'arraybuffer'
-aReq.onload = reqListener;
-aReq.open("get", "samples/Onze-20 - Joao e Grazi.mp3", true);
-aReq.send();
-
-function reqListener() {
-    context.decodeAudioData(aReq.response, function (buffer) {
-        soundBuffer = buffer;
-        playSound(soundBuffer)
-    });
-}
-
-function playSound(buffer) {
+var getData = function () {
     source = context.createBufferSource();
-    source.buffer = soundBuffer;
     gainNode = context.createGain();
     source.connect(gainNode);
     gainNode.connect(context.destination);
     gainNode.gain.value = volumeControl.value;
-    source.start(0);
+
+    if (soundBuffer) {
+        source.buffer = soundBuffer;
+    } else {
+        var aReq = new XMLHttpRequest();
+        aReq.responseType = 'arraybuffer'
+        aReq.onload = reqListener;
+        aReq.open("get", "samples/Onze-20 - Joao e Grazi.mp3", true);
+        aReq.send();
+        function reqListener() {
+            context.decodeAudioData(aReq.response, function (buffer) {
+                soundBuffer = buffer
+                source.buffer = soundBuffer;
+            });
+        }
+    }
+}
+// wire up buttons
+volumeControl.oninput = function () {
+    gainNode.gain.value = this.value;
 }
 
-// wire up buttons
-volumeControl.oninput = function(){
-    gainNode.gain.value = this.value;
+play.onclick = function () {
+    getData();
+    source.start(0);
+    play.setAttribute('disabled', 'disabled');
+}
+
+stop.onclick = function () {
+    source.stop(0);
+    play.removeAttribute('disabled');
 }
