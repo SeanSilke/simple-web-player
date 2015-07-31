502c6de1bbc41a003ba2d7bbc61a420d23b1705a
Sergey Orlov <orlovMail@gmail.com>
2015-07-31 23:59:53 +0300

    Read file as ArrayBuffer

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
 var fileInput = document.querySelector('.file-input');
 var dropZone = document.querySelector('.drop-zone')
 
-//fileInput.onchange = handleFiles;
+
 var handleFileSelect = function (evt) {
     evt.stopPropagation();
     evt.preventDefault();
     var files = evt.target.files || evt.dataTransfer.files
     var file = files[0]
-    console.log(file.name, file.type)
 
     var reader = new FileReader();
+    reader.onload = function(e){
+        console.log(e.target.result)
+        console.log("result ArrayBuffer",reader.result)
+    }
+
+    reader.readAsArrayBuffer(file);
 };
 
 function handleDragOver(evt) {
